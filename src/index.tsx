6c8eb4132539c4104e3668c7d9ba87b1364552f5
Ibnu Putra <azxarblog@gmail.com>
2024-03-31 22:11:25 +0700

    chore: initialing project commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,14 @@
+import * as React from 'react';
+import { View, Text } from 'react-native';
+
 export function multiply(a: number, b: number): Promise<number> {
   return Promise.resolve(a * b);
+}
+
+export function simpleText() {
+  return (
+    <View>
+      <Text>Simple text v1.0</Text>
+    </View>
+  );
 }
